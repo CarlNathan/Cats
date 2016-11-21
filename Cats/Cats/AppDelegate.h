c6bdec17a5c20234257dcc266090de6a51eebd14
CarlNathan <c.n.udren@gmail.com>
2016-11-21 14:52:19 -0500

    Inital Commit

---
+++ Cats/Cats/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cats
+//
+//  Created by Carl Udren on 11/21/16.
+//  Copyright © 2016 Carl Udren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cats/Cats/ViewController.m
@@ -0,0 +1,39 @@
+//
+//  ViewController.m
+//  Cats
+//
+//  Created by Carl Udren on 11/21/16.
+//  Copyright © 2016 Carl Udren. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonWasPressed:(id)sender {
+    //submit tag and fetch photos
+    //networking code
+    //completion
+    //launch collection view
+}
+
+
+@end
